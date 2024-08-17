5ce100fd2de15f1864afb0d34a5b8a1064d1c458
Matthew Kaye <mattkaye79@gmail.com>
2024-08-17 18:13:21 -0400

    Work on the index page.

---
+++ src/customTypes.ts
@@ -0,0 +1,16 @@
+export const EFFORT_LEVEL = {
+  beginner: {
+    color: "green",
+    emoji: "😅",
+  },
+
+  intermediate: {
+    color: "orange",
+    emoji: "😓",
+  },
+
+  expert: {
+    color: "red",
+    emoji: "🥵",
+  },
+};
