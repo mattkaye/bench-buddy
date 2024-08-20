1ac04cda165035ddc9587c82c7082a6957c267d3
Matthew Kaye <mattkaye79@gmail.com>
2024-08-20 17:48:17 -0400

    Clean up and work on single page.

--- src/customTypes.ts
+++ src/customTypes.ts
@@ -8,7 +8,7 @@ export type Exercise = {
     mechanic: string;
     equipment: string;
     primaryMuscles: string[];
-    secondaryMuscles?: string[];
+    secondaryMuscles: string[];
     instructions: string[];
     category: string[];
     images: string[];
