9c42846db1e1283fb7787a6b0e3b308d25040d1e
Matthew Kaye <mattkaye79@gmail.com>
2024-09-12 13:31:05 -0400

    Removing all Astro stuff

--- src/content/config.ts
+++
@@ -1,9 +0,0 @@
-import { defineCollection } from "astro:content";
-
-const exerciseCollection = defineCollection({
-  type: "data",
-});
-
-export const collections = {
-  exercise: exerciseCollection,
-};
